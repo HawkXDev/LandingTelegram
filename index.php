576315277c3edcc90c09e6583ce32dec1bcaa3a9
sokolserg <sergsokolkin@gmail.com>
2023-09-08 10:20:38 +0300

    Implement the skeleton, introduction and first question

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Тест путешественника</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <p>Вы можете получить консультацию абсолютно бесплатно без обращения в офис</p>
+    <h1>Пройдите тест за 1 минуту и узнайте, какие страны лучше всего подходят для вашего следующего путешествия!</h1>
+
+    <div class="demo-text">Демонстрационный режим</div>
+
+    <div class="additional-text">
+        <p>Сразу после заполнения вы получите</p>
+        100% точные рекомендации для вашего путешествия
+    </div>
+
+    <div class="additional-text-2">
+        Узнайте, какие страны лучше всего подходят<br>для вашего следующего путешествия
+    </div>
+
+    <form class="form">
+        <div class="question-block">
+            <p><span class="question-number">1. </span>Вопрос</p>
+            <div class="question-answers">
+                <div class="answer-option"><input type="radio" name="name" id="name1"> <label for="name1">Ответ 1</label></div>
+                <div class="answer-option"><input type="radio" name="name" id="name2"> <label for="name2">Ответ 2</label></div>
+                <div class="answer-option"><input type="radio" name="name" id="name3"> <label for="name3">Ответ 3</label></div>
+                <div class="answer-option"><input type="radio" name="name" id="name4"> <label for="name4">Ответ 4</label></div>
+                <div class="answer-option"><input type="radio" name="name" id="name5"> <label for="name5">Ответ 5</label></div>
+            </div>
+        </div>
+
+        <button type="submit">Узнать</button>
+    </form>
+</div>
+
+</body>
+</html>
