90d6b41c60c82f67b06930955820a3adc4fc2a8c
sokolserg <sergsokolkin@gmail.com>
2023-09-28 16:22:47 +0300

    fix ios border-radius issue

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Тест путешественника</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.03">
 </head>
 <body>
 
